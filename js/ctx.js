deca5647d4250e5263be261340752ed9e05f594c
Keenan Brock <keenan@thebrocks.net>
2015-03-15 02:18:48 -0400

    introduce transform (for origin)
    use hash for point

--- js/ctx.js
+++ js/ctx.js
@@ -29,23 +29,55 @@ Ctx.prototype = {
       this.ctx.stroke();
     return this;
   },
-  cir: function(x,y,r, start, end, opaque) {
+  cir: function(pt,r, start, end, opaque) {
     if (opaque === undefined) {
       opaque = true;
     }
     // (x,y) center, radius, start angle, end angle, anticlockwise
-    this.ctx.arc(x, y, r, (start || 0) * Math.PI, (end || 2) * Math.PI, opaque);
+    this.ctx.arc(pt.x, pt.y, r, (start || 0) * Math.PI, (end || 2) * Math.PI, opaque);
     this.draw();
     return this;
   },
-  go: function(x, y) {
-    this.ctx.moveTo(x, y);
+  go: function(pt) {
+    this.ctx.moveTo(pt.x, pt.y);
     return this;
   },
   line: function() {
-    for (var i = 0; i < arguments.length;) {
-      this.ctx.lineTo(arguments[i++], arguments[i++]);
+    for (var i = 0; i < arguments.length; i++) {
+      this.ctx.lineTo(arguments[i].x, arguments[i].y);
     }
     return this.draw();
+  }
+};
+
+function Transpose(attrs) {
+  this.attrs = attrs;
+}
+
+Transpose.prototype = {
+  t: function(obj) {
+    var ret = {};
+    for (var k in obj) {
+      ret[k] = obj[k];
+    }
+    for (k in this.attrs) {
+      if (ret[k] !== undefined) {
+        ret[k] += this.attrs[k];
+      }
+    }
+    return ret;
+  },
+  flip: function(obj, f) {
+    f = f || ['x'];
+    var ret = {};
+    for (var k in obj) {
+      ret[k] = obj[k];
+    }
+    for (var i = 0; i < f.length ; i++) {
+      k = f[i];
+      if (ret[k] !== undefined)
+        ret[k] = - ret[k] + 2 * this.attrs[k];
+    }
+    return obj;
   }
 };
